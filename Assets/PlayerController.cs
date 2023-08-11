b9fa2a86998c6fa925fe2950b8f632e3ebc456f8
muto.sota <>
2023-08-11 14:09:04 +0900

    fix bug

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -24,17 +24,16 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * this.JumpForce);
+        }
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
 
-            int key = 0;
-            if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-            if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        float speedX = Mathf.Abs(this.rigid2D.velocity.x);
 
-            float speedX = Mathf.Abs(this.rigid2D.velocity.x);
-
-            if (speedX < this.MaxWalkSpeed)
-            {
-                this.rigid2D.AddForce(transform.right * key * this.WalkForce);
-            }
+        if (speedX < this.MaxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.WalkForce);
         }
 
         if (transform.position.y < -10.0f)

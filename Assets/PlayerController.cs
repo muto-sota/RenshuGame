525e0cecfb9c731fedbeef76bd2ffbe968ac9a4a
muto.sota <>
2023-08-11 13:22:04 +0900

    change scene

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -20,7 +21,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * this.JumpForce);
 
@@ -34,12 +35,17 @@ public class PlayerController : MonoBehaviour
             {
                 this.rigid2D.AddForce(transform.right * key * this.WalkForce);
             }
+        }
 
+        if (transform.position.y < -10.0f)
+        {
+            SceneManager.LoadScene("GameScene");
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("CLEAR!");
+        SceneManager.LoadScene("ClearScene");
     }
 }

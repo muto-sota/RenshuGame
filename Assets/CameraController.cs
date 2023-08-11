c231be886f967f161516639d1715ee1f43de27b0
muto.sota <>
2023-08-11 13:07:40 +0900

    set camera.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.Find("kuma");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(transform.position.x, playerPos.y, transform.position.z);
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -37,4 +37,9 @@ public class PlayerController : MonoBehaviour
 
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("CLEAR!");
+    }
 }
